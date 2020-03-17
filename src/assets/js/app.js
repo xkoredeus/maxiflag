cb7413f1fe55176bd4bef8e2a9f826bcba73de2e
xkoredeus <xkored@gmail.com>
2020-03-17 15:44:11 +0400

    feat: blog, contacts, order, service, standarts pages

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -43,7 +43,41 @@ $(() => {
         animationEffect   : 'slide-in-in',
         touch : false
     });
-    $('.menu__close').on('click', function () {
+    $('.menu__close').on('click', function menuCloseBtnClick() {
         $.fancybox.close();
+    });
+});
+$(() => {
+    $('.upload__file').change(function() {
+        let filename = [];
+        for (let i = 0; i < $(this).get(0).files.length; ++i) {
+            filename.push($(this).get(0).files[i].name.slice(0, 18) + '...');
+        }
+        $(this).next('.filename').text(filename.join(', '));
+    });
+});
+$(() => {
+    $('.js-service-page__item-btn').on('click', function serviceItemBtnClick() {
+        const top = $(this).offset().top;
+        $('body,html').animate({scrollTop: top - 100}, 300);
+        $(this)
+            .parent('.service-page__item')
+            .toggleClass('active')
+            .find('.service-page__item-in')
+            .slideToggle();
+    });
+});
+$(() => {
+    $('.tabs__content-item:not(:first-child)').hide();
+    $('.tabs__list-item:first-child').addClass('active');
+    $('.tabs__list > .tabs__list-item').click(function tabListLiClick() {
+        if (!($(this).hasClass('active'))) {
+            const thisLi = $(this);
+            const numLi = thisLi.index();
+            thisLi.addClass('active').siblings().removeClass('active');
+            thisLi.parent().next().children('div').hide()
+                .eq(numLi)
+                .fadeIn('slow');
+        }
     });
 });
